654c712ade3226ac2cc06e1497e49883ecf31853
Hamed Matari <hamed.m@arianalabs.io>
2022-11-19 22:47:21 +0330

    job page logic

--- pages/job/[id].jsx
+++ pages/job/[id].jsx
@@ -1,67 +1,93 @@
-import CustomButton from 'utils/UI/CustomButton';
+import CustomButton from "utils/UI/CustomButton";
 
-import styles from 'styles/job.module.scss';
-import Typography from 'utils/UI/Typography';
+import styles from "styles/job.module.scss";
+import Typography from "utils/UI/Typography";
+import useGetJob from "utils/apiCalls/useGetJob";
+import { useRouter } from "next/router";
+import calculateDate from "utils/UI/calculateDay";
 
 function Job() {
+  const router = useRouter();
+  const { id } = router.query;
+  const { isLoading, data } = useGetJob({ id });
+  console.log("data", data);
+  if (isLoading) return <Typography variant="bold">Loading...</Typography>;
   return (
-    <div className='container-md'>
+    <div className="container-md">
       {/* Header */}
       <div className={styles.header}>
-        <div className={styles['header-box-container']}>
-          <div className={styles['header-box']}>scoot</div>
+        <div className={styles["header-box-container"]}>
+          <div
+            className={styles["header-box"]}
+            // style={{ backgroundColor: "red !important" }}
+            style={{ backgroundColor: data.logoBackground }}
+          >
+            <img src={data.logo} alt="logo" />
+          </div>
         </div>
 
-        <div className={`${styles['header-content']} ${styles['card-padding']}`}>
-          <div className={`${styles['header-text']}`}>
-            <Typography variant='bold' className={styles.title}>
-              Scoot
+        <div
+          className={`${styles["header-content"]} ${styles["card-padding"]}`}
+        >
+          <div className={`${styles["header-text"]}`}>
+            <Typography variant="bold" className={styles.title}>
+              {data.company}
             </Typography>
-            <Typography variant='light' className={styles['text-light']}>
-              http://exapmple.com/scoot
+            <Typography variant="light" className={styles["text-light"]}>
+              {data.apply}
             </Typography>
           </div>
-          <CustomButton variant='light' className={styles['compony-btn']}>
-            Compony Site
+          <CustomButton variant="light" className={styles["compony-btn"]}>
+            Company Site
           </CustomButton>
         </div>
       </div>
 
       {/* body */}
-      <div className={`${styles.body} ${styles['card-padding']}`}>
+      <div className={`${styles.body} ${styles["card-padding"]}`}>
         {/* header */}
-        <div className={`${styles['body-content']} mb-2`}>
-          <div className={styles['body-content-left']}>
-            <div className={styles.time}>4H . Full Time</div>
-            <Typography variant='bold' className={styles['body-title']}>
-              Senior Software Engineer
+        <div className={`${styles["body-content"]} mb-2`}>
+          <div className={styles["body-content-left"]}>
+            <div className={styles.time}>
+              {calculateDate(data.postedAt)} . {data.contract}
+            </div>
+            <Typography variant="bold" className={styles["body-title"]}>
+              {data.position}
             </Typography>
-            <div className={styles.country}>United Kingdom</div>
+            <div className={styles.country} style={{ marginTop: "10px" }}>
+              <Typography variant="middle">{data.location}</Typography>
+            </div>
           </div>
 
-          <CustomButton className={styles['body-content-right']}>Apply Now</CustomButton>
+          <CustomButton className={styles["body-content-right"]}>
+            Apply Now
+          </CustomButton>
+        </div>
+        <p>{data.description}</p>
+
+        <div>
+          <Typography variant="bold">Requirement</Typography>
+          <p>{data?.requirements?.content}</p>
         </div>
         <div>
-          <Typography variant='bold'>Title</Typography>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid commodi molestiae
-            possimus mollitia voluptatum ratione neque placeat provident voluptatibus dolore, quo
-            cum, vero hic cumque nemo temporibus ipsam fugiat dignissimos.
-          </p>
+          <Typography variant="bold">What you will do</Typography>
+          <p>{data?.requirements?.content}</p>
         </div>
       </div>
 
-      <div className={styles['action-bottom']}>
-        <div className={styles['action-bottom-container']}>
-          <div className={styles['body-content']}>
-            <div className={styles['body-content-left']}>
-              <Typography variant='bold' className={styles['body-title']}>
-                Senior Software Engineer
+      <div className={styles["action-bottom"]}>
+        <div className={styles["action-bottom-container"]}>
+          <div className={styles["body-content"]}>
+            <div className={styles["body-content-left"]}>
+              <Typography variant="bold" className={styles["body-title"]}>
+                {data.position}
               </Typography>
-              <div className={styles.time}>scoot</div>
+              <div className={styles.time}>{data.company}</div>
             </div>
 
-            <CustomButton className={styles['body-content-right']}>Apply Now</CustomButton>
+            <CustomButton className={styles["body-content-right"]}>
+              Apply Now
+            </CustomButton>
           </div>
         </div>
       </div>
