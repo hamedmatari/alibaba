6737d09f2b9559adbdbf52350beaf8d36df285a3
Hamed Matari <hamed.m@arianalabs.io>
2022-11-19 20:38:41 +0330

    checkbox controlled value

--- utils/UI/Checkbox.jsx
+++ utils/UI/Checkbox.jsx
@@ -1,19 +1,15 @@
 import { useEffect, useState } from "react";
 import styles from "styles/Checkbox.module.scss";
 
-function Checkbox({ checked = false }) {
-  const [state, setState] = useState(checked);
-  // useEffect(() => {
-  //     setState(checked)
-  // },[checked])
+function Checkbox({ value = "false", onChange }) {
   return (
     <div className={styles.main}>
       <label className={styles.container}>
-        sa
+        .
         <input
           type="checkbox"
-          checked={state}
-          onChange={(e) => setState(e.target.checked)}
+          checked={value}
+          onChange={(e) => onChange(e.target.checked)}
         />
         <span className={styles.checkmark}></span>
       </label>
