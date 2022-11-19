a60c0ad0f02d8c851ad852140d6660e066d628d9
Hamed Matari <hamed.m@arianalabs.io>
2022-11-19 22:48:14 +0330

    ADD link to itemCard component

--- components/ItemCard.jsx
+++ components/ItemCard.jsx
@@ -1,27 +1,30 @@
+import Link from "next/link";
 import styles from "styles/ItemCard.module.scss";
 import calculateDate from "utils/UI/calculateDay";
 import Typography from "utils/UI/Typography";
 
 function ItemCard({ item }) {
   return (
-    <main className={styles.main}>
-      <div
-        className={styles.logo}
-        style={{ backgroundColor: item.logoBackground }}
-      >
-        <img src={item.logo} alt="logo" />
-      </div>
-      <div className={styles.content}>
-        <Typography variant="light">
-          {calculateDate(item.postedAt)} ago • {item.contract}
-        </Typography>
-        <Typography variant="bold">{item.position}</Typography>
-        <Typography variant="light">{item.company}</Typography>
-      </div>
-      <div className={styles.location}>
-        <Typography variant="middle">{item.location}</Typography>
-      </div>
-    </main>
+    <Link href={`/job/${item.id}`}>
+      <main className={styles.main}>
+        <div
+          className={styles.logo}
+          style={{ backgroundColor: item.logoBackground }}
+        >
+          <img src={item.logo} alt="logo" />
+        </div>
+        <div className={styles.content}>
+          <Typography variant="light">
+            {calculateDate(item.postedAt)} ago • {item.contract}
+          </Typography>
+          <Typography variant="bold">{item.position}</Typography>
+          <Typography variant="light">{item.company}</Typography>
+        </div>
+        <div className={styles.location}>
+          <Typography variant="middle">{item.location}</Typography>
+        </div>
+      </main>
+    </Link>
   );
 }
 
