ee269f8ce2e45061cf54b03d2072b525a6a53e13
Hamed Matari <hamed.m@arianalabs.io>
2022-11-19 14:47:10 +0330

    ADD Home page and desing app structure

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
-import '../styles/globals.css'
+import "styles/globals.scss";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Jabama | code challenge</title>
+        <meta name="description" content="Hamed Matari code challenge" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
